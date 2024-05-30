94b78dbc1ef12894d1d9bbb11ae48bd1d328f4b1
electroblats <adam.laurell2@gmail.com>
2024-05-30 21:19:52 +0200

    1

--- Automated-GuitarAmpModelling/dist_model_recnet.py
+++ Automated-GuitarAmpModelling/dist_model_recnet.py
@@ -84,7 +84,7 @@ def main(args):
         cuda = 1
 
     # Set up training optimiser + scheduler + loss fcns and training info tracker
-    optimiser = torch.optim.Adam(network.parameters(), lr=args.learn_rate, weight_decay=1e-3)
+    optimiser = torch.optim.Adam(network.parameters(), lr=args.learn_rate, weight_decay=1e-4)
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimiser, 'min', factor=0.5, patience=10, verbose=True)
     loss_functions = training.LossWrapper(args.loss_fcns, args.pre_filt)
     train_track = training.TrainTrack()
